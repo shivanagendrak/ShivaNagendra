15ed74e0431c62f48bde06d8f3236dfe0bedaf93
shivanagendrak <shivaaman751@gmail.com>
2023-06-09 16:08:54 -0400

    Adding commands  = hi , Ping

--- bot.py
+++ bot.py
@@ -4,9 +4,21 @@ from config import TOKEN
 
 bot = commands.Bot(command_prefix="#" , case_insensitive = True , intents=discord.Intents.all(), self_bot = True)
 
+@bot.command()
+async def hi(ctx):
+    await ctx.send(f"Hello {ctx.message.author}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"Latency is {round((bot.latency)*1000)}ms")
+
+
 @bot.event
 async def on_ready():
     print("Bot is Ready!")
+
+
+
 
 
 bot.run(TOKEN)
