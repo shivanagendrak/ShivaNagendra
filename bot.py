5b8a145a62404898b0666abd87cd71f72d002a5b
shivanagendrak <shivaaman751@gmail.com>
2023-06-09 22:33:47 -0400

    update

--- bot.py
+++ bot.py
@@ -1,24 +1,14 @@
 import discord
 from discord.ext import commands
 from config import TOKEN
-
 bot = commands.Bot(command_prefix="#" , case_insensitive = True , intents=discord.Intents.all(), self_bot = True)
-
 @bot.command()
 async def hi(ctx):
-    await ctx.send(f"Hello {ctx.message.author}")
-
+    await ctx.send(f"Hello  {ctx.message.author} ")
 @bot.command()
 async def ping(ctx):
-    await ctx.send(f"Latency is {round((bot.latency)*1000)}ms")
-
-
+    await ctx.send(f"Latency is{round((bot.latency)*1000)} ms")
 @bot.event
 async def on_ready():
     print("Bot is Ready!")
-
-
-
-
-
 bot.run(TOKEN)
